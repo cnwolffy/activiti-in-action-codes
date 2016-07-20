101ae9ee317ca6d9370e39e6744f32d513f37a2d
灰太狼 <zhouyang@yiji.com>
2016-07-20 11:12:52 +0800

    重构依赖版本

---
+++ chapter20-rest/src/main/java/me/kafeitu/activiti/web/chapter20/RestServiceController.java
@@ -0,0 +1,20 @@
+/*
+ * www.yiji.com Inc.
+ * Copyright (c) 2014 All Rights Reserved
+ */
+
+/*
+ * 修订记录:
+ * zhouyang@yiji.com 2016-07-20 11:07 创建
+ *
+ */
+package me.kafeitu.activiti.web.chapter20;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhouyang@yiji.com
+ */
+@RestController("/service")
+public class RestServiceController {
+}
